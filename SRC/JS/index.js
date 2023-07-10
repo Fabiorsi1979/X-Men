4e59fe5c34d605104710983e5202c3a427693164
fabiorsi <frsilvaconsultoria@gmail.com>
2023-07-09 21:27:12 -0300

    alterado

--- SRC/JS/index.js
+++ SRC/JS/index.js
@@ -3,23 +3,24 @@ const personagens = document.querySelectorAll('.personagem');
 personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter',() => {
 
-        if (window.innerWidth > 450){
-            window.scrollTo({top: 0, behavior: "smooth"});
-        }
+    if (window.innerWidth < 450px){
+        window.scrollTo({top: 0, behavior: "smooth"});
+    }
 
-        const personagemSelecionado = document.querySelector('.selecionado');
-        personagemSelecionado.classList.remove('selecionado');
-        personagem.classList.add('selecionado');
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+    personagem.classList.add('selecionado');
 
-        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
 
-        const idPersonagem = personagem.attributes.id.value;
-        imagemPersonagemGrande.src = `./SRC/imagens/card-${idPersonagem}.png`;
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./SRC/imagens/card-${idPersonagem}.png`;
 
-        const nomePersonagem = document.getElementById('nome-personagem');
-        nomePersonagem.innerText= personagem.getAttribute('data-name');
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText= personagem.getAttribute('data-name');
 
-        const descricaoPersonagem = document.getElementById('descricao-personagem');
-        descricaoPersonagem.innerText= personagem.getAttribute('data-description')
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText= personagem.getAttribute('data-description')
+
     })
 })
